855a5fcac690f049c48c566fde3c99a3e764843f
Mariam Nasser <96596668+MariamNasser45@users.noreply.github.com>
2023-05-10 16:46:24 +0300

    Add Identity Tables

--- Server/TalabatReplica/ECommerce/Program.cs
+++ Server/TalabatReplica/ECommerce/Program.cs
@@ -1,3 +1,8 @@
+using ECommerce.Data;
+using ECommerce.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerce
 {
     public class Program
@@ -7,6 +12,19 @@ namespace ECommerce
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            //mapping values of JWT section in json file to properties in JWT class
+
+            builder.Configuration.GetSection("JWT").Get<JWTData>();
+
+            var connectionString = builder.Configuration.GetConnectionString("MyConn");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            ////Define Identity Services
+            //builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
