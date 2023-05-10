dcf3eedcf14d59359a9e5ab6b35bc12112758daa
Peter Ezzat <peterezzat97@gmail.com>
2023-05-10 17:36:45 +0300

    all done

--- 1- Server/TalabatReplica/ECommerce/Helpers/JWTData.cs
+++ 1- Server/TalabatReplica/ECommerce/Helpers/JWTData.cs
@@ -2,13 +2,15 @@
 {
     public class JWTData
     {
-     
 
             //define properties same to which defined in json file
 
             public string Key { get; set; }
+
             public string Issuer { get; set; }
+        
             public string Audience { get; set; }
+            
             public double DurationInDays { get; set; }
         
     }
