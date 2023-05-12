9dfc9dcf06c1c92932f24ba1a8d2689c744cfbc2
Peter Ezzat <peterezzat97@gmail.com>
2023-05-12 19:19:05 +0300

    Added default roles

---
+++ 1- Server/TalabatReplica/ECommerce.DAL/Services/RegisterServices.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerce.DAL.Services
+{
+    public static class RegisterServices
+    {
+        public static async Task<IServiceCollection> RegisterIdentityService( this IServiceCollection service )
+        {
+            #region Dfault Roles
+            var _serviceProvider = service.BuildServiceProvider( );
+            var scope = _serviceProvider.CreateScope( );
+            var services = scope.ServiceProvider;
+            // created logger service
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>( );
+            var _logger = loggerFactory.CreateLogger( "app" );
+            // created role manager service
+            var _roleManager = services.GetRequiredService<RoleManager<IdentityRole>>( );
+            await Seed.DefaultRole.SeedRoles( _roleManager );
+            _logger.Log( LogLevel.Information , "registered default roles" );
+            return service;
+
+
+            #endregion
+        }
+    }
+}
--- 1- Server/TalabatReplica/ECommerce/Program.cs
+++ 1- Server/TalabatReplica/ECommerce/Program.cs
@@ -1,4 +1,4 @@
-using ECommerce;
+using ECommerce.DAL.Services;
 using ECommerce.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -7,47 +7,52 @@ namespace ECommerce
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main( string[ ] args )
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder( args );
 
             // Add services to the container.
 
             //mapping values of JWT section in json file to properties in JWT class
 
-            builder.Configuration.GetSection("JWT").Get<JWTData>();
+            builder.Configuration.GetSection( "JWT" ).Get<JWTData>( );
 
-            var connectionString = builder.Configuration.GetConnectionString("MyConn");
+            var connectionString = builder.Configuration.GetConnectionString( "MyConn" );
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<ApplicationDbContext>( options =>
+                options.UseSqlServer( connectionString ) );
 
-            ////Define Identity Services
-            //builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            builder.Services.AddControllers();
+            //Define Identity Services
+            builder.Services.AddIdentity<IdentityUser , IdentityRole>( )
+                .AddEntityFrameworkStores<ApplicationDbContext>( );
+
+            await builder.Services.RegisterIdentityService( );
+
+            builder.Services.AddControllers( );
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddEndpointsApiExplorer( );
+            builder.Services.AddSwaggerGen( );
 
-            var app = builder.Build();
+            var app = builder.Build( );
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if ( app.Environment.IsDevelopment( ) )
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwagger( );
+                app.UseSwaggerUI( );
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection( );
 
-            app.UseAuthorization();
+            app.UseAuthorization( );
 
 
-            app.MapControllers();
+            app.MapControllers( );
 
-            app.Run();
+
+
+            app.Run( );
         }
     }
 }
