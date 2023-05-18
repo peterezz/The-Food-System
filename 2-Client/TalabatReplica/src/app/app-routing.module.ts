624ff8627a40e717de30a06214c3655052704b7d
AhmedAbuhilal <ahmedabuhilal3@gmail.com>
2023-05-19 01:07:31 +0300

    cart and payment page

--- 2-Client/TalabatReplica/src/app/app-routing.module.ts
+++ 2-Client/TalabatReplica/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { RestaurantComponent } from './Components/restaurant/restaurant.component';
 import { LoginComponent } from './Components/login/login.component';
 import { ErrorComponent } from './Components/error/error.component';
+import { CheckOutComponent } from './Components/check-out/check-out.component';
 
 const routes: Routes = [
   {path:"", component: RestaurantComponent},
   {path:"login", component: LoginComponent},
+  {path:"checkOut", component: CheckOutComponent},
   {path:"**", component: ErrorComponent}
 
 ];
