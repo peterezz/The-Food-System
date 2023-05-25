8608ddea0f189bdee1af06df0797c7f14fa7cbbc
Peter Ezzat <peterezzat97@gmail.com>
2023-05-25 13:22:29 +0300

    Started Email service

---
+++ 1- Server/TalabatReplica/ECommerce.DAL/Reposatory/RepoServices/MailingService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.DAL.Reposatory.Repo;
+using Microsoft.AspNetCore.Http;
+
+namespace ECommerce.DAL.Reposatory.RepoServices
+{
+    public class MailingService : IEmailService
+    {
+        public async Task SendEmailAsync( string mailTo , string body , IList<IFormFile> attachments = null )
+        {
+
+        }
+    }
+}
