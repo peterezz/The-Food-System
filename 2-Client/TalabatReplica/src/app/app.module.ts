191c1a5b129b39882be00c8dfd8cc54149f6c0eb
Mariam Nasser <96596668+MariamNasser45@users.noreply.github.com>
2023-05-26 13:52:11 +0300

    try login services

--- 2-Client/TalabatReplica/src/app/app.module.ts
+++ 2-Client/TalabatReplica/src/app/app.module.ts
@@ -14,19 +14,13 @@ import { AllResturantsComponent } from './Components/all-resturants/all-resturan
 import { AllResturantsItemsComponent } from './Components/all-resturants-items/all-resturants-items.component';
 import { SingleproductComponent } from './Components/singleproduct/singleproduct.component';
 import { CartComponent } from './Components/cart/cart.component';
-// import { MatSidenavModule} from'@angular/material/sidenav'
-// import {MatIconModule} from'@angular/material/icon'
-// import {MatTooltipModule} from'@angular/material/tooltip'
-
 import { PaymentComponent } from './Components/payment/payment.component';
 import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
 import { SetnewpasswordComponent } from './Components/setnewpassword/setnewpassword.component';
 import { ContactComponent } from './Components/contact/contact.component';
 import { CategoryItemsComponent } from './Components/category-items/category-items.component';
 import { RouterLink } from '@angular/router';
-// import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
-// import { SetnewpasswordComponent } from './Components/setnewpassword/setnewpassword.component';
-// import { ContactComponent } from './Components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
@@ -57,16 +51,7 @@ import { RouterLink } from '@angular/router';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    // MatSidenavModule,
-    // MatIconModule,
-    // MatTooltipModule
-
-    RouterLink,
-    // MatSidenavModule,
-    // MatIconModule,
-    // MatTooltipModule
-
-
+    RouterLink
 
   ],
   providers: [],
