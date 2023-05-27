2545159afcc481e5a785692979d2509734edf5ec
Somayazaid2 <somayazaid88@gmail.com>
2023-05-27 16:44:33 +0300

    Restaurant Client Views

--- 2-Client/TalabatReplica/src/app/Components/all-resturants/all-resturants.component.ts
+++ 2-Client/TalabatReplica/src/app/Components/all-resturants/all-resturants.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestuarantService } from 'src/app/Services/restuarant.service';
 
 @Component({
   selector: 'app-all-resturants',
   templateUrl: './all-resturants.component.html',
   styleUrls: ['./all-resturants.component.css']
 })
-export class AllResturantsComponent {
+export class AllResturantsComponent implements OnInit {
+  constructor(public service : RestuarantService){}
+  Restarunts :any ;
+  ngOnInit(): void {
+        this.service.GetAllRestuarants().subscribe({
+          next:(data)=>{this.Restarunts=data;console.log(data);},
+          error:(error)=>{console.log(error);}
+        });
+  }
 
 }
